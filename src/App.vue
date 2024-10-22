121514853789126a5bb193572132299459d696e6
Kevin Nørby Andersen <kevin@superultra.dk>
2024-10-22 19:00:53 +0200

    move completions into text.js

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,16 @@
 <script setup>
 import { ref, nextTick } from 'vue'
+import { createCompletionsChat } from './text'
 
 const content = ref('')
 const BTN_TEXT = 'Submit 🚀'
 const res = ref('✅ The answer will be displayed here.')
 const btnText = ref(BTN_TEXT)
 
-async function createCompletionsChat() {
-  try {
-    btnText.value = 'Thinking...🤔'
-
-    const userMessages = [{ role: 'user', content: content.value }]
-
-    const requestData = JSON.stringify({
-      model: 'gpt-3.5-turbo',
-      messages: userMessages,
-      stream: true,
-    })
-
-    const fetchOptions = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${import.meta.env.VITE_OPEN_API_KEY}`,
-      },
-      body: requestData,
-    }
-
-    const response = await fetch('https://api.openai.com/v1/chat/completions', fetchOptions)
-    const reader = response.body.getReader()
-    res.value = ''
-
-    while (true) {
-      const { done, value } = await reader.read()
-      if (done) break
-
-      const chunkStr = new TextDecoder('utf-8').decode(value)
-      const lines = chunkStr
-        .split('\n')
-        .filter((line) => line !== '' && line.length > 0)
-        .map((line) => line.replace(/^data: /, '').trim())
-        .filter((line) => line !== '[DONE]')
-        .map((line) => JSON.parse(line))
-      for (const line of lines) {
-        const {
-          choices: [
-            {
-              delta: { content },
-            },
-          ],
-        } = line
-        if (content) {
-          res.value += content
-        }
-      }
-    }
-    console.log('Stream ended.')
-  } catch (error) {
-    console.error(error)
-    res.value = error.response.data.error.message
-  } finally {
-    btnText.value = BTN_TEXT
-  }
-}
-
-const askAi = () => {
-  createCompletionsChat()
+const askAi = async () => {
+  btnText.value = `Thinking...🤔`
+  await createCompletionsChat(res, content)
+  btnText.value = BTN_TEXT
 }
 </script>
 
